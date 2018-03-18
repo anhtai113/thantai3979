83925e0915ea399927ea83474ef06beda4ac2a24
DESKTOP-UTAT1SK\vitop <vitop.tainguyenvan@gmail.com>
2018-03-18 15:06:18 +0700

    user

--- backend/app/Http/Controllers/Users/UserController.php
+++ backend/app/Http/Controllers/Users/UserController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 class UserController extends Controller
 {
+	
     public function index(){
     	$data = User::all();
     	return view('users.index',compact('data'));
