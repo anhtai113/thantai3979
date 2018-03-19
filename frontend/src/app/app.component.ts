2f93989bc95cd456c110680765b97a790e919d9c
q.thanhphong72@gmail.com <q.thanhphong72@gmail.com>
2018-03-19 13:56:12 +0700

    register + api

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
-
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Thantai3979';
+
+  registerTitle = 'Register';
+  showRegisterMessage = false;
+  registerMessageClass = 'alert alert-warning';
+  registerMessage = '';
+  loading = false;
+  http;
+
+  constructor(http: Http) {
+    this.http = http;
+  }
+
+  submitRegisterForm(event, fullname, email, password, cfpassword, idModal) {
+    this.loading = true;
+    event.preventDefault();
+    if (password !== cfpassword) {
+      this.showMessage(true, 'warning', 'Password did not match');
+    } else {
+      this.http.get('http://google.com').subscribe( (res: Response) => {
+        console.log(res);
+      });
+      this.showMessage(true, 'success', 'Success');
+    }
+  }
+
+  showMessage(isShow, type, message) {
+    this.showRegisterMessage = isShow;
+    this.registerMessage = message;
+
+    switch (type) {
+      case 'danger': {
+        this.registerMessageClass = 'alert alert-danger';
+        break;
+      }
+      case 'warning': {
+        this.registerMessageClass = 'alert alert-warning';
+        break;
+      }
+      case 'success': {
+        this.registerMessageClass = 'alert alert-success';
+        break;
+      }
+      case 'info': {
+        this.registerMessageClass = 'alert alert-info';
+        break;
+      }
+    }
+    this.loading = false;
+    setTimeout ( () => {
+      this.showRegisterMessage = false;
+    } , 4000);
+  }
 }
